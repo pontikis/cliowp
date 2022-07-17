f5996e6ff59814c317a1f6b5095074ff8daf8d65
Christos Pontikis <pontikis@gmail.com>
2022-07-17 17:14:40 +0300

    load goggle font

--- theme/functions.php
+++ theme/functions.php
@@ -7,6 +7,7 @@
 
 function get_front_end_assets()
 {
+    wp_enqueue_style('load_google_font_ubuntu', '//fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;1,300&display=swap');
     wp_enqueue_style('load_main_css_style', get_stylesheet_uri());
 }
 
