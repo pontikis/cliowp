3827cedd179e66036e5c6a6dae6d96b11a2d4347
Christos Pontikis <pontikis@gmail.com>
2022-07-17 18:40:14 +0300

    important web page attributes

--- theme/header.php
+++ theme/header.php
@@ -6,12 +6,14 @@
  */
 ?>
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes(); ?>>
 
 <head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
 </head>
 
-<body>
+<body <?php body_class(); ?>>
     <!-- CUSTOM CODE HERE -->
     <p><a href="<?php echo site_url(); ?>">Home page</a></p>
