27f2d236fe06b7f56f0e587cb8da14102fd5a0b2
Christos Pontikis <pontikis@gmail.com>
2022-07-31 18:17:47 +0300

    archive

---
+++ wp_dev_starter_theme/archive.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The main template file for displaying the Home page
+ * (or any other page if a specific template does not exist for it).
+ *
+ * This is the Famous WordPress Loop
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ */
+?>
+<?php get_header(); ?>
+
+<h1><?php the_archive_title(); ?>
+</h1>
+<p><?php the_archive_description(); ?>
+</p>
+
+<?php while (have_posts()): ?>
+
+<?php the_post(); ?>
+
+<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+
+<div>
+    <?php get_template_part('parts/topic_meta'); ?>
+</div>
+
+<?php the_excerpt(); ?>
+
+<small><a href="<?php the_permalink(); ?>">More &raquo; </a></small>
+
+<hr>
+
+<?php endwhile; ?>
+
+<p><?php echo paginate_links(); ?>
+</p>
+
+<?php get_footer();
