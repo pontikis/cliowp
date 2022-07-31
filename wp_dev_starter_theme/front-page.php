fbf6e5100b2005e7c61dad37a9aea8b63d9b4ad2
Christos Pontikis <pontikis@gmail.com>
2022-07-31 18:26:28 +0300

    front-page.php

--- wp_dev_starter_theme/front-page.php
+++ wp_dev_starter_theme/front-page.php
@@ -7,14 +7,6 @@
 ?>
 <?php get_header(); ?>
 
-<?php while (have_posts()): ?>
-
-<?php the_post(); ?>
-
-<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-<?php the_content(); ?>
-<hr>
-
-<?php endwhile; ?>
+<h1>Home</h1>
 
 <?php get_footer();
