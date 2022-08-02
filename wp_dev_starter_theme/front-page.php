e76a255a1f26aea1de9e67394ed2d707b6c0c7e7
Christos Pontikis <pontikis@gmail.com>
2022-08-02 18:26:55 +0300

    WP_query at home

--- wp_dev_starter_theme/front-page.php
+++ wp_dev_starter_theme/front-page.php
@@ -4,9 +4,24 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  */
+
+$recent_posts = new WP_Query([
+    'posts_per_page' => 3
+]);
 ?>
 <?php get_header(); ?>
 
 <h1>Home</h1>
+
+<?php while ($recent_posts->have_posts()): ?>
+<?php $recent_posts->the_post(); ?>
+
+<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+<p><?php echo wp_trim_words(get_the_content(), 15); ?> <a
+        href="<?php the_permalink(); ?>">Read more</a></p>
+
+<?php endwhile; ?>
+
+<?php wp_reset_postdata(); ?>
 
 <?php get_footer();
