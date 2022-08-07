f75336bbdcd387d40220485e2337e1ed0591f4b1
Christos Pontikis <pontikis@gmail.com>
2022-08-07 15:13:57 +0300

    search form

--- wp_dev_starter_theme/parts/topic_meta.php
+++ wp_dev_starter_theme/parts/topic_meta.php
@@ -1,4 +1,5 @@
+<?php if ('post' === get_post_type()): ?>
 <div class="post-meta-container">
-<?php the_author_posts_link();  ?> | <?php the_time('j-n-Y');  ?> | <?php echo get_the_category_list(', ');  ?>
+    <?php the_author_posts_link(); ?> | <?php the_time('j-n-Y'); ?> | <?php echo get_the_category_list(', '); ?>
 </div>
-
+<?php endif;
---
+++ wp_dev_starter_theme/search.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The main template file for displaying the Search results
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ */
+
+$results       = new WP_Query("s=$s&showposts=-1");
+$count_results = $results->post_count;
+wp_reset_query();
+?>
+<?php get_header(); ?>
+
+<h1>Search results</h1>
+
+<h2>Search query: &ldquo;<?php echo esc_html(get_search_query()); ?>&rdquo;</h2>
+
+<?php if ($count_results): ?>
+<h3>Results: <?php echo $count_results ?></h3>
+<?php else: ?>
+<h3>No results for this query...</h3>
+<?php endif; ?>
+
+<?php get_template_part('parts/famous_wordpress_loop'); ?>
+
+<p><?php echo paginate_links(); ?>
+</p>
+
+<?php get_footer();
