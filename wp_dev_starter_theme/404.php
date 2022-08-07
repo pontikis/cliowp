abcda085f2fc73064d6b089cfe76f7674c8a40d0
Christos Pontikis <pontikis@gmail.com>
2022-08-07 15:18:00 +0300

    404 page

---
+++ wp_dev_starter_theme/404.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The template file for displaying 404
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ */
+?>
+<?php get_header(); ?>
+
+<h1>Page not found...</h1>
+
+<?php get_footer();
