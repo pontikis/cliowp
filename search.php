7e543076fbd01048df742e36915c7cc335f32cb7
Christos Pontikis <pontikis@gmail.com>
2022-08-11 09:36:15 +0300

    improve search

--- search.php
+++ search.php
@@ -11,19 +11,33 @@ wp_reset_query();
 ?>
 <?php get_header(); ?>
 
-<h1><?php _e('Search results', 'cliowp'); ?></h1>
+<h1><?php _e('Search results', 'cliowp'); ?>
+</h1>
 
-<h2><?php _e('Search query', 'cliowp'); ?> &ldquo;<?php echo esc_html(get_search_query()); ?>&rdquo;</h2>
+<?php if (trim(get_search_query())): ?>
+
+<h2><?php _e('Search query', 'cliowp') . ':'; ?>
+    &ldquo;<?php echo esc_html(get_search_query()); ?>&rdquo;</h2>
 
 <?php if ($count_results): ?>
-<h3><?php _e('Results', 'cliowp'); ?> <?php echo $count_results ?></h3>
+<h3><?php _e('Results', 'cliowp'); echo ':'; ?>
+    <?php echo $count_results ?>
+</h3>
 <?php else: ?>
-<h3><?php _e('No results for this query...', 'cliowp'); ?></h3>
+<h3><?php _e('No results for this query', 'cliowp'); echo '...'; ?>
+</h3>
 <?php endif; ?>
 
 <?php get_template_part('parts/famous_wordpress_loop'); ?>
 
 <p><?php echo paginate_links(); ?>
 </p>
+
+<?php else: ?>
+
+<h3><?php _e('Please enter a search term', 'cliowp'); echo '...'; ?>
+</h3>
+
+<?php endif; ?>
 
 <?php get_footer();
