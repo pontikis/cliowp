81494ba83ce86f7d47831a5d4483b2b377f8e10c
Christos Pontikis <pontikis@gmail.com>
2022-08-16 14:28:33 +0300

    improve front page

--- front-page.php
+++ front-page.php
@@ -11,14 +11,28 @@ $recent_posts = new WP_Query([
 ?>
 <?php get_header(); ?>
 
-<h1><?php _e('Home', 'cliowp'); ?></h1>
+<h1><?php _e('Home', 'cliowp'); ?>
+</h1>
+
+<h2><?php _e('Custom content', 'cliowp'); ?>
+</h2>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, tempora numquam ab sapiente omnis ea aspernatur
+    placeat aperiam hic exercitationem, facilis iusto cum unde voluptatum, debitis expedita delectus! Doloribus,
+    voluptatum?</p>
+
+<hr>
+
+<h2><?php _e('Custom query', 'cliowp'); ?>
+</h2>
 
 <?php while ($recent_posts->have_posts()): ?>
 <?php $recent_posts->the_post(); ?>
 
-<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 <p><?php echo wp_trim_words(get_the_content(), 15); ?> <a
-        href="<?php the_permalink(); ?>"><?php _e('Read more', 'cliowp'); ?></a></p>
+        href="<?php the_permalink(); ?>"><?php _e('Read more', 'cliowp'); ?></a>
+</p>
 
 <?php endwhile; ?>
 
