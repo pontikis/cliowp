e0797c22a5b54cfd4f1a41e1269dff94fff0a471
Christos Pontikis <pontikis@gmail.com>
2022-12-05 12:26:54 +0200

    Bug fix

--- template-parts/topic-meta.php
+++ template-parts/topic-meta.php
@@ -5,9 +5,16 @@
  * @package template
  */
 
+$allow_html = array(
+	'a' => array(
+		'href' => array(),
+		'rel'  => array(),
+	),
+);
+
 if ( 'post' === get_post_type() ) : ?>
 <div class="cliowp-post-meta-container">
-	<?php the_author_posts_link(); ?> | <?php the_time( 'j-n-Y' ); ?> | <?php echo esc_html( get_the_category_list( ', ' ) ); ?>
+	<?php the_author_posts_link(); ?> | <?php the_time( 'j-n-Y' ); ?> | <?php echo wp_kses( get_the_category_list( ', ' ), $allow_html ); ?>
 </div>
 	<?php
 endif;
